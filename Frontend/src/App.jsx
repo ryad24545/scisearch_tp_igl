c35b60a7e739cef3b3538d9ec81bcd967b4db271
unknown <TAIBI Ryad Brahim>
2023-12-25 18:40:15 +0000

    inscription

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,12 +8,16 @@ import Landing from './Pages/Landing';
 import Navbar from './Components/Navbar1';
 import Resultat from './Pages/Resultat';
 import Part3 from './Pages/login';
+import Admin from './Pages/Admin';
+import Inscription from './Pages/Inscription';
+
 const App = () => {
   
   return (
     <Router>
       <div>
-       <Part3/>
+        <Navbar/>
+       <Inscription/>
       </div>
     </Router>
   );
--- Frontend/src/Pages/Admin.jsx
+++ Frontend/src/Pages/Admin.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.png'
+import logo from '../logo.png'
 
 const Admin = () => {
   return (
     <div class="buttons_container">
        <div className='img_admin'><img src={logo}></img></div>
         <div ><button className='buttons1'>Gerer moderateurs</button></div>
-        <div ><button className='buttons2'> upload Fichier </button></div>
+        <div ><button className='buttons2'> Upload Fichier </button></div>
 </div>
   );
 };
